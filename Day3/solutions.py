69fe25717f3198525f91dcb794b041345619bdf7
Ansh Choudhary <32743873+AnshChoudhary@users.noreply.github.com>
2024-01-24 14:27:35 +0530

    Create solutions.py

---
+++ Day3/solutions.py
@@ -0,0 +1,23 @@
+# 238. Product of Array Except Self
+# Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        res = [1] * len(nums)
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+
+        return res 
+
