3f36f91cb01b9d3a6c56b507124a6971b6334ef6
Ansh Choudhary <32743873+AnshChoudhary@users.noreply.github.com>
2024-01-24 15:21:02 +0530

    Update solutions.py

--- Day3/solutions.py
+++ Day3/solutions.py
@@ -46,3 +46,24 @@ class Solution(object):
                 rows[r].add(board[r][c])
                 squares[(r//3, c//3)].add(board[r][c])
         return True
+
+# 128. Longest Consecutive Sequence
+# Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence. You must write an algorithm that runs in O(n) time.
+
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        numSet = set(nums)
+        longest = 0
+
+        for n in numSet:
+            if (n - 1) not in numSet:
+                length = 1
+                while (n + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+        return longest
+
