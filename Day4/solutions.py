b3189feae94c57724f85f3691c8535fb1071e72d
Ansh Choudhary <32743873+AnshChoudhary@users.noreply.github.com>
2024-01-26 19:28:32 +0530

    Create solutions.py

---
+++ Day4/solutions.py
@@ -0,0 +1,21 @@
+# 167. Two Sum
+# Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. 
+# Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        l, r = 0, len(numbers) - 1
+        while l < r:
+            curSum = numbers[l] + numbers[r]
+
+            if curSum > target:
+                r -= 1
+            elif curSum < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
+        return []
