6458556105a9fa56acd30a84a49f3a3e9c3e0d00
Ansh Choudhary <32743873+AnshChoudhary@users.noreply.github.com>
2024-01-26 19:55:09 +0530

    Update solutions.py

--- Day4/solutions.py
+++ Day4/solutions.py
@@ -19,3 +19,33 @@ class Solution(object):
             else:
                 return [l + 1, r + 1]
         return []
+
+# 15. 3Sum
+# Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
+        nums.sort()
+        res = []
+        
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i-1]:
+                continue
+            
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l-1] and l<r:
+                        l += 1
+        return res
