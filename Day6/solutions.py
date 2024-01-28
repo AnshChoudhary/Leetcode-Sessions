0d09155a8091bd78d665224c9314a7a0dd88acdc
Ansh Choudhary <32743873+AnshChoudhary@users.noreply.github.com>
2024-01-28 17:06:32 +0530

    Update solutions.py

--- Day6/solutions.py
+++ Day6/solutions.py
@@ -19,3 +19,40 @@ class Solution(object):
             else:
                 stack.append(c)
         return True if not stack else False
+
+# 155.Min Stack
+# Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.minStack[-1]
