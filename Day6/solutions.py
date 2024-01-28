fe94f6c70a0d47b22d0cecb1513d897a7dda2141
Ansh Choudhary <32743873+AnshChoudhary@users.noreply.github.com>
2024-01-28 20:43:42 +0530

    Update solutions.py

--- Day6/solutions.py
+++ Day6/solutions.py
@@ -88,3 +88,22 @@ class Solution(object):
             else:
                 stack.append(int(i))
         return stack[0]
+
+
+# 739. Daily Temperatures
+# Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature.
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        res = [0] * len(temperatures)
+        stack = []
+
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackInd = stack.pop()
+                res[stackInd] = i - stackInd
+            stack.append((t, i))
+        return res
