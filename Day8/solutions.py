b4b2c8ac264a7da4e15e93e7c7b185b76f727274
Ansh Choudhary <32743873+AnshChoudhary@users.noreply.github.com>
2024-02-09 09:38:40 +0400

    Create solutions.py

---
+++ Day8/solutions.py
@@ -0,0 +1,22 @@
+# Binary Search
+#
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = l + ((r - l) // 2 )
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                return m
+        return -1
+
