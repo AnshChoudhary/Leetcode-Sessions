438def780a81b3387e154e85dc5ed44a6fea7233
Ansh Choudhary <32743873+AnshChoudhary@users.noreply.github.com>
2024-02-20 11:35:45 +0400

    Update solutions.py

--- Day10/solutions.py
+++ Day10/solutions.py
@@ -1,4 +1,4 @@
-# 3. Longest Substring without repeating characters
+# 121. Best time to buy or sell stocks
 # Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 class Solution(object):
     def maxProfit(self, prices):
@@ -18,4 +18,24 @@ class Solution(object):
             r += 1
         return maxP
 
-# 
+# 3. Longest Substring without Repeating Characters
+# Given a string s, find the length of the longest substring without repeating characters.
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        charSet = set()
+        l = 0
+        res = 0
+
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            res = max(res, r - l + 1)
+        return res
+
